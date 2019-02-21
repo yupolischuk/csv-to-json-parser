21246e4f5b1bc0627375ea36039a05917b754987
yupolischuk <yumuscletank@gmail.com>
2019-02-21 20:44:30 +0200

    Parser ready to go

---
+++ README.md
@@ -0,0 +1,8 @@
+Installation
+----
+Open console in folder with cloned repository
+	run npm install
+
+Usage
+----
+	node app.js ArchiveFileName.zip
---
+++ app.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+const AdmZip = require('adm-zip');
+const csv = require('csvtojson/v2');
+
+
+async function run() {
+    const archivePath = process.argv[2];
+
+    if (typeof archivePath != 'undefined') {
+        let dataSourceDir = extractFiles('./' + archivePath);
+        let filePathes = await getFilePathes(dataSourceDir);
+
+        for (let i = 0; i < filePathes.length; i++) {
+            parseFile(filePathes[i]);
+        }        
+    } else {
+        console.log('Set file to parse');
+    }
+};
+run();
+
+
+function extractFiles(archFilePath) {
+    let zip = new AdmZip(archFilePath);
+    zip.extractAllTo('./tmp/', true);
+
+    return __dirname + '/tmp/';
+}
+
+function getFilePathes(targetDir) {
+    let filePathes = [];
+    return new Promise(function(resolve, reject) {
+        fs.readdir(targetDir, (err, files) => {
+            files.forEach(file => {
+                filePathes.push(__dirname + '/tmp/' + file);
+            });
+            resolve(filePathes);
+        });
+    });
+};
+
+function parseFile(filePath) {
+    const outputFile = path.basename(filePath).split('.').slice(0, -1).join('.') + '.json';
+    const file = fs.createWriteStream(outputFile);
+    let separator = '';
+    
+    file.write('[');
+    csv({delimiter: '||'})
+    .fromFile(filePath)
+    .on('data', (data) => {
+        userObj = parseUser(JSON.parse(data));
+        file.write(separator + JSON.stringify(userObj));
+        if (!separator) {
+            separator = ',';
+        }
+    })
+    .on('done', (err) => {
+        file.write(']');
+    })
+}
+
+function parseUser(user) {
+    return {
+        name: user.last_name + ' ' + user.first_name,
+        phone: user.phone.replace(/\s*[()-]\s*/g, ''),
+        person: {
+            firstName: {
+                type: user.first_name
+            },
+            lastName: {
+                type: user.last_name
+            }
+        },
+        amount: user.amount,
+        date: user.date.split('/').reverse().join('-'),
+        costCenterNum: user.cc.replace(/\s*[A-Z]\s*/g, ''),
+    }
+}
